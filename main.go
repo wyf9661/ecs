9e66340140e4c5f460d50f80dcfcbbb2cb3f29da
wangyifan <wangyifan@acoinfo.com>
2024-12-04 19:50:15 +0800

    add tag for container tarball

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"ecs/parser"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -14,8 +15,8 @@ func main() {
 	parser.InitImageConfig()
 	parser.InitGlobalImageInfos()
 
-	if len(os.Args) < 2 {
-		log.Fatalf("Usage:\n\tecs create <bundle>\n\tecs save <bundle> <image.tar>\nother cmd is not supported for now!")
+	if len(os.Args) <= 2 {
+		log.Fatalf("Usage:\n\tecs create <bundle>\n\tecs save <bundle> <image.tar:version>/<image.tar>")
 	}
 
 	input := os.Args[1]
@@ -26,11 +27,28 @@ func main() {
 			log.Println("Error generating RootFS:", err)
 		}
 	case "save":
-		if err := cmd.EcsSave(os.Args[2], os.Args[3]); err != nil {
+
+		if len(os.Args) <= 3 {
+			log.Fatalf("Usage:\n\tecs save <bundle> <image.tar:version>/<image.tar>")
+		}
+
+		parts := strings.Split(os.Args[3], ":")
+		var name, version string
+
+		if len(parts) > 1 {
+			name = strings.TrimSpace(parts[0])
+			version = strings.TrimSpace(parts[1])
+			log.Printf("set container tarball version with %s.", version)
+		} else {
+			name = os.Args[3]
+			version = "latest"
+			log.Println("No version configuration, `latest` will be set as default.")
+		}
+
+		if err := cmd.EcsSave(os.Args[2], name, version); err != nil {
 			log.Println("Error saving Image:", err)
 		}
 	default:
-		log.Fatalf("Usage:\n\tecs create <bundle>\n\tecs save <bundle> <image.tar>\nother cmd is not supported for now!")
-		os.Exit(1)
+		log.Fatalf("Usage:\n\tecs create <bundle>\n\tecs save <bundle> <image.tar:version>/<image.tar>")
 	}
 }
