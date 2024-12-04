6b6e70bf6ccd9eeb3867c182c5b5177e1c3b37e1
wangyifan <wangyifan@acoinfo.com>
2024-12-04 19:50:15 +0800

    add tag for container tarball

--- cmd/ecs_save.go
+++ cmd/ecs_save.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -112,7 +113,7 @@ func ecsSaveCreateLayer(bundlePath string, workDir string) (string, error) {
 	return sha256sums, nil
 }
 
-func ecsSaveImageConfig(bundlePath string, workDir string, layerSha256 string) (string, error) {
+func ecsSaveImageConfig(workDir string, layerSha256 string) (string, error) {
 
 	now := time.Now()
 	formattedTime := now.Format(time.RFC3339Nano)
@@ -125,57 +126,57 @@ func ecsSaveImageConfig(bundlePath string, workDir string, layerSha256 string) (
 	parser.GlobalImageConfig.Config.Entrypoint = parser.GlobalConfigStruct.Process.Args
 	parser.GlobalImageConfig.Config.WorkingDir = parser.GlobalConfigStruct.Process.Cwd
 
-	parser.GlobalImageConfig.Config.Labels["hostname"] = parser.GlobalConfigStruct.Hostname
+	parser.GlobalImageConfig.Config.Labels[`hostname`] = parser.GlobalConfigStruct.Hostname
 
 	for i := 0; i < len(parser.GlobalConfigStruct.Mounts); i++ {
-		parser.GlobalImageConfig.Config.Labels["mounts."+strconv.Itoa(i)+".destination"] = parser.GlobalConfigStruct.Mounts[i].Destination
-		parser.GlobalImageConfig.Config.Labels["mounts."+strconv.Itoa(i)+".source"] = parser.GlobalConfigStruct.Mounts[i].Source
-		parser.GlobalImageConfig.Config.Labels["mounts."+strconv.Itoa(i)+".options"] = parser.GlobalConfigStruct.Mounts[i].Options[0]
+		parser.GlobalImageConfig.Config.Labels[`mounts.`+strconv.Itoa(i)+`.destination`] = parser.GlobalConfigStruct.Mounts[i].Destination
+		parser.GlobalImageConfig.Config.Labels[`mounts.`+strconv.Itoa(i)+`.source`] = parser.GlobalConfigStruct.Mounts[i].Source
+		parser.GlobalImageConfig.Config.Labels[`mounts.`+strconv.Itoa(i)+`.options`] = parser.GlobalConfigStruct.Mounts[i].Options[0]
 	}
 
 	for i := 0; i < len(parser.GlobalConfigStruct.Sylixos.Commands); i++ {
-		parser.GlobalImageConfig.Config.Labels["sylixos.commands."+strconv.Itoa(i)] = parser.GlobalConfigStruct.Sylixos.Commands[i]
+		parser.GlobalImageConfig.Config.Labels[`sylixos.commands.`+strconv.Itoa(i)] = parser.GlobalConfigStruct.Sylixos.Commands[i]
 	}
 
 	for i := 0; i < len(parser.GlobalConfigStruct.Sylixos.Devices); i++ {
-		parser.GlobalImageConfig.Config.Labels["sylixos.devices."+strconv.Itoa(i)+".path"] = parser.GlobalConfigStruct.Sylixos.Devices[i].Path
-		parser.GlobalImageConfig.Config.Labels["sylixos.devices."+strconv.Itoa(i)+".access"] = parser.GlobalConfigStruct.Sylixos.Devices[i].Access
+		parser.GlobalImageConfig.Config.Labels[`sylixos.devices.`+strconv.Itoa(i)+`.path`] = parser.GlobalConfigStruct.Sylixos.Devices[i].Path
+		parser.GlobalImageConfig.Config.Labels[`sylixos.devices.`+strconv.Itoa(i)+`.access`] = parser.GlobalConfigStruct.Sylixos.Devices[i].Access
 	}
 
-	parser.GlobalImageConfig.Config.Labels["sylixos.resources.cpu.highestPrio"] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.Cpu.HighestPrio)
-	parser.GlobalImageConfig.Config.Labels["sylixos.resources.cpu.lowestPrio"] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.Cpu.LowestPrio)
+	parser.GlobalImageConfig.Config.Labels[`sylixos.resources.cpu.highestPrio`] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.Cpu.HighestPrio)
+	parser.GlobalImageConfig.Config.Labels[`sylixos.resources.cpu.lowestPrio`] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.Cpu.LowestPrio)
 
-	parser.GlobalImageConfig.Config.Labels["sylixos.resources.memory.kheapLimit"] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.Memory.KheapLimit)
-	parser.GlobalImageConfig.Config.Labels["sylixos.resources.memory.memoryLimitMB"] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.Memory.MemoryLimitMB)
+	parser.GlobalImageConfig.Config.Labels[`sylixos.resources.memory.kheapLimit`] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.Memory.KheapLimit)
+	parser.GlobalImageConfig.Config.Labels[`sylixos.resources.memory.memoryLimitMB`] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.Memory.MemoryLimitMB)
 
-	parser.GlobalImageConfig.Config.Labels["sylixos.resources.disk.limitMB"] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.Disk.LimitMB)
+	parser.GlobalImageConfig.Config.Labels[`sylixos.resources.disk.limitMB`] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.Disk.LimitMB)
 
-	parser.GlobalImageConfig.Config.Labels["sylixos.resources.kernelObject.deviceLimit"] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.KernelObject.DeviceLimit)
-	parser.GlobalImageConfig.Config.Labels["sylixos.resources.kernelObject.dlopenLibraryLimit"] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.KernelObject.DlopenLibraryLimit)
-	parser.GlobalImageConfig.Config.Labels["sylixos.resources.kernelObject.eventLimit"] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.KernelObject.EventLimit)
-	parser.GlobalImageConfig.Config.Labels["sylixos.resources.kernelObject.eventSetLimit"] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.KernelObject.EventSetLimit)
-	parser.GlobalImageConfig.Config.Labels["sylixos.resources.kernelObject.msgQueueLimit"] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.KernelObject.MsgQueueLimit)
-	parser.GlobalImageConfig.Config.Labels["sylixos.resources.kernelObject.partitionLimit"] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.KernelObject.PartitionLimit)
-	parser.GlobalImageConfig.Config.Labels["sylixos.resources.kernelObject.posixMqueueLimit"] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.KernelObject.PosixMqueueLimit)
-	parser.GlobalImageConfig.Config.Labels["sylixos.resources.kernelObject.regionLimit"] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.KernelObject.RegionLimit)
-	parser.GlobalImageConfig.Config.Labels["sylixos.resources.kernelObject.rmsLimit"] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.KernelObject.RmsLimit)
-	parser.GlobalImageConfig.Config.Labels["sylixos.resources.kernelObject.socketLimit"] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.KernelObject.SocketLimit)
-	parser.GlobalImageConfig.Config.Labels["sylixos.resources.kernelObject.srtpLimit"] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.KernelObject.SrtpLimit)
-	parser.GlobalImageConfig.Config.Labels["sylixos.resources.kernelObject.threadLimit"] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.KernelObject.ThreadLimit)
-	parser.GlobalImageConfig.Config.Labels["sylixos.resources.kernelObject.threadPoolLimit"] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.KernelObject.ThreadPoolLimit)
-	parser.GlobalImageConfig.Config.Labels["sylixos.resources.kernelObject.threadVarLimit"] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.KernelObject.ThreadVarLimit)
-	parser.GlobalImageConfig.Config.Labels["sylixos.resources.kernelObject.timerLimit"] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.KernelObject.TimerLimit)
-	parser.GlobalImageConfig.Config.Labels["sylixos.resources.kernelObject.xsiipcLimit"] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.KernelObject.XsiipcLimit)
+	parser.GlobalImageConfig.Config.Labels[`sylixos.resources.kernelObject.deviceLimit`] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.KernelObject.DeviceLimit)
+	parser.GlobalImageConfig.Config.Labels[`sylixos.resources.kernelObject.dlopenLibraryLimit`] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.KernelObject.DlopenLibraryLimit)
+	parser.GlobalImageConfig.Config.Labels[`sylixos.resources.kernelObject.eventLimit`] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.KernelObject.EventLimit)
+	parser.GlobalImageConfig.Config.Labels[`sylixos.resources.kernelObject.eventSetLimit`] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.KernelObject.EventSetLimit)
+	parser.GlobalImageConfig.Config.Labels[`sylixos.resources.kernelObject.msgQueueLimit`] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.KernelObject.MsgQueueLimit)
+	parser.GlobalImageConfig.Config.Labels[`sylixos.resources.kernelObject.partitionLimit`] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.KernelObject.PartitionLimit)
+	parser.GlobalImageConfig.Config.Labels[`sylixos.resources.kernelObject.posixMqueueLimit`] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.KernelObject.PosixMqueueLimit)
+	parser.GlobalImageConfig.Config.Labels[`sylixos.resources.kernelObject.regionLimit`] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.KernelObject.RegionLimit)
+	parser.GlobalImageConfig.Config.Labels[`sylixos.resources.kernelObject.rmsLimit`] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.KernelObject.RmsLimit)
+	parser.GlobalImageConfig.Config.Labels[`sylixos.resources.kernelObject.socketLimit`] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.KernelObject.SocketLimit)
+	parser.GlobalImageConfig.Config.Labels[`sylixos.resoarDirectoryurces.kernelObject.srtpLimit`] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.KernelObject.SrtpLimit)
+	parser.GlobalImageConfig.Config.Labels[`sylixos.resources.kernelObject.threadLimit`] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.KernelObject.ThreadLimit)
+	parser.GlobalImageConfig.Config.Labels[`sylixos.resources.kernelObject.threadPoolLimit`] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.KernelObject.ThreadPoolLimit)
+	parser.GlobalImageConfig.Config.Labels[`sylixos.resources.kernelObject.threadVarLimit`] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.KernelObject.ThreadVarLimit)
+	parser.GlobalImageConfig.Config.Labels[`sylixos.resources.kernelObject.timerLimit`] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.KernelObject.TimerLimit)
+	parser.GlobalImageConfig.Config.Labels[`sylixos.resources.kernelObject.xsiipcLimit`] = strconv.Itoa(parser.GlobalConfigStruct.Sylixos.Resources.KernelObject.XsiipcLimit)
 
-	parser.GlobalImageConfig.Config.Labels["sylixos.network.ftpdEnable"] = fmt.Sprintf("%t", parser.GlobalConfigStruct.Sylixos.Network.FtpdEnable)
-	parser.GlobalImageConfig.Config.Labels["sylixos.network.telnetdEnable"] = fmt.Sprintf("%t", parser.GlobalConfigStruct.Sylixos.Network.TelnetdEnable)
-	parser.GlobalImageConfig.Config.Labels["sylixos.network.sshdEnable"] = fmt.Sprintf("%t", parser.GlobalConfigStruct.Sylixos.Network.SshdEnable)
+	parser.GlobalImageConfig.Config.Labels[`sylixos.network.ftpdEnable`] = fmt.Sprintf("%t", parser.GlobalConfigStruct.Sylixos.Network.FtpdEnable)
+	parser.GlobalImageConfig.Config.Labels[`sylixos.network.telnetdEnable`] = fmt.Sprintf("%t", parser.GlobalConfigStruct.Sylixos.Network.TelnetdEnable)
+	parser.GlobalImageConfig.Config.Labels[`sylixos.network.sshdEnable`] = fmt.Sprintf("%t", parser.GlobalConfigStruct.Sylixos.Network.SshdEnable)
 
-	parser.GlobalImageConfig.Rootfs.Type = "layers"
-	parser.GlobalImageConfig.Rootfs.DiffIds[0] = "sha256:" + layerSha256
+	parser.GlobalImageConfig.Rootfs.Type = `layers`
+	parser.GlobalImageConfig.Rootfs.DiffIds[0] = `sha256:` + layerSha256
 
 	parser.GlobalImageConfig.History[0].Created = formattedTime
-	parser.GlobalImageConfig.History[0].CreatedBy = "ecs (go version for built)"
+	parser.GlobalImageConfig.History[0].CreatedBy = `ecs (go version for built)`
 	parser.GlobalImageConfig.History[0].EmptyLayer = false
 
 	jsonData, err := json.MarshalIndent(parser.GlobalImageConfig, "", "    ")
@@ -207,7 +208,7 @@ func ecsSaveImageConfig(bundlePath string, workDir string, layerSha256 string) (
 }
 
 func ecsSaveCreateRepositories(workDir string, layerSha256 string, imageName string, imageTag string) error {
-	result := fmt.Sprintf("{\n\t\"%s\":{\n\t\t\"%s\":\"%s\"\n\t}\n}", imageName, imageTag, layerSha256)
+	result := fmt.Sprintf("{\n\t\"%s\":{\n\t\t\"%s\":\"%s\"\n\t}\n}", strings.TrimSuffix(imageName, ".tar"), imageTag, layerSha256)
 
 	if err := common.CreateFile(workDir+"/repositories", result); err != nil {
 		return err
@@ -219,7 +220,7 @@ func ecsSaveCreateRepositories(workDir string, layerSha256 string, imageName str
 func ecsSaveCreateManifest(workDir string, layerSha256 string, configSha256 string, imageName string, imageTag string) error {
 
 	parser.GlobalImageInfos[0].Config = configSha256 + ".json"
-	parser.GlobalImageInfos[0].RepoTags[0] = imageTag
+	parser.GlobalImageInfos[0].RepoTags[0] = strings.TrimSuffix(imageName, ".tar") + ":" + imageTag
 	parser.GlobalImageInfos[0].Layers[0] = layerSha256 + "/layer.tar"
 
 	jsonData, err := json.MarshalIndent(parser.GlobalImageInfos, "", "    ")
@@ -236,7 +237,7 @@ func ecsSaveCreateManifest(workDir string, layerSha256 string, configSha256 stri
 	return nil
 }
 
-func EcsSave(bundlePath string, imageName string) error {
+func EcsSave(bundlePath string, imageName string, imageVersion string) error {
 
 	_, err := os.Stat(bundlePath)
 	if os.IsNotExist(err) {
@@ -253,16 +254,16 @@ func EcsSave(bundlePath string, imageName string) error {
 		log.Println("Failed to create layer!")
 	}
 
-	configSha256, err := ecsSaveImageConfig(bundlePath, "./ecs_temp", layerSha256)
+	configSha256, err := ecsSaveImageConfig("./ecs_temp", layerSha256)
 	if err != nil {
 		log.Println("Failed to create layer!")
 	}
 
-	if err := ecsSaveCreateRepositories("./ecs_temp", layerSha256, imageName, "latest"); err != nil {
+	if err := ecsSaveCreateRepositories("./ecs_temp", layerSha256, imageName, imageVersion); err != nil {
 		log.Println("Failed to create repositories!")
 	}
 
-	if err := ecsSaveCreateManifest("./ecs_temp", layerSha256, configSha256, imageName, "latest"); err != nil {
+	if err := ecsSaveCreateManifest("./ecs_temp", layerSha256, configSha256, imageName, imageVersion); err != nil {
 		log.Println("Failed to create repositories!")
 	}
 
